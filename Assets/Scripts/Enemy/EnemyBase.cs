a23074f5411800d966659fe913b0e0a90aaa8d74
Ryusei1115 <tekunikaru22@gmail.com>
2022-05-02 13:55:17 +0900

    WIP

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class EnemyBase : MonoBehaviour
 {
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    // Update is called once per frame
     void Update()
     {
         
